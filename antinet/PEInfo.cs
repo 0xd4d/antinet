978b248a6512d74aa952053812a83af3c56fb714
de4dot <de4dot@gmail.com>
2013-01-02 21:11:42 +0100

    Add PEInfo

---
+++ antinet/PEInfo.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * This code was written by de4dot@gmail.com but is placed in the public domain.
+ * Use at your own risk. You don't need to credit me, but it wouldn't hurt either. :)
+ * 
+ * Official site: https://bitbucket.org/0xd4d/antinet
+ */
+
+using System;
+using System.Text;
+
+namespace antinet {
+	class PEInfo {
+		IntPtr imageBase;
+		IntPtr imageEnd;
+		IntPtr sectionsAddr;
+		int numSects;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="addr">Address of a PE image</param>
+		public PEInfo(IntPtr addr) {
+			this.imageBase = addr;
+			Initialize();
+		}
+
+		unsafe void Initialize() {
+			byte* p = (byte*)imageBase;
+			p += *(uint*)(p + 0x3C);	// Get NT headers
+			p += 4 + 2;	// Skip magic + machine
+			numSects = *(ushort*)p;
+			p += 2 + 0x10;	// Skip the rest of file header
+			bool is32 = *(ushort*)p == 0x010B;
+			uint sizeOfImage = *(uint*)(p + 0x38);
+			imageEnd = new IntPtr((byte*)imageBase + sizeOfImage);
+			p += is32 ? 0x60 : 0x70;	// Skip optional header
+			p += 0x10 * 8;		// Skip data dirs
+			sectionsAddr = new IntPtr(p);
+		}
+
+		/// <summary>
+		/// Checks whether the address is within the image
+		/// </summary>
+		/// <param name="addr">Address</param>
+		public unsafe bool IsValidImageAddress(IntPtr addr) {
+			return IsValidImageAddress((void*)addr, 0);
+		}
+
+		/// <summary>
+		/// Checks whether the address is within the image
+		/// </summary>
+		/// <param name="addr">Address</param>
+		/// <param name="size">Number of bytes</param>
+		public unsafe bool IsValidImageAddress(IntPtr addr, uint size) {
+			return IsValidImageAddress((void*)addr, size);
+		}
+
+		/// <summary>
+		/// Checks whether the address is within the image
+		/// </summary>
+		/// <param name="addr">Address</param>
+		public unsafe bool IsValidImageAddress(void* addr) {
+			return IsValidImageAddress(addr, 0);
+		}
+
+		/// <summary>
+		/// Checks whether the address is within the image
+		/// </summary>
+		/// <param name="addr">Address</param>
+		/// <param name="size">Number of bytes</param>
+		public unsafe bool IsValidImageAddress(void* addr, uint size) {
+			if (addr < (void*)imageBase)
+				return false;
+			if (addr >= (void*)imageEnd)
+				return false;
+
+			if (size != 0) {
+				if ((byte*)addr + size < (void*)addr)
+					return false;
+				if ((byte*)addr + size > (void*)imageEnd)
+					return false;
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Finds a section
+		/// </summary>
+		/// <param name="name">Name of section</param>
+		/// <param name="sectionStart">Updated with start address of section</param>
+		/// <param name="sectionSize">Updated with size of section</param>
+		/// <returns><c>true</c> on success, <c>false</c> on failure</returns>
+		public unsafe bool FindSection(string name, out IntPtr sectionStart, out uint sectionSize) {
+			var nameBytes = Encoding.UTF8.GetBytes(name + "\0\0\0\0\0\0\0\0");
+			for (int i = 0; i < numSects; i++) {
+				byte* p = (byte*)sectionsAddr + i * 0x28;
+				if (!CompareSectionName(p, nameBytes))
+					continue;
+
+				sectionStart = new IntPtr((byte*)imageBase + *(uint*)(p + 12));
+				sectionSize = Math.Max(*(uint*)(p + 8), *(uint*)(p + 16));
+				return true;
+			}
+
+			sectionStart = IntPtr.Zero;
+			sectionSize = 0;
+			return false;
+		}
+
+		static unsafe bool CompareSectionName(byte* sectionName, byte[] nameBytes) {
+			for (int i = 0; i < 8; i++) {
+				if (*sectionName != nameBytes[i])
+					return false;
+				sectionName++;
+			}
+			return true;
+		}
+
+		/// <inheritdoc/>
+		public override string ToString() {
+			return string.Format("{0:X8} - {1:X8}", (ulong)imageBase.ToInt64(), (ulong)imageEnd.ToInt64());
+		}
+	}
+}
