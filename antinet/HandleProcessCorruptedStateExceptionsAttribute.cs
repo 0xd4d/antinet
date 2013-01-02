acc356f52679cfc1d4cdd1a1b762c667e3fe68d4
de4dot <de4dot@gmail.com>
2013-01-02 21:10:56 +0100

    Add HandleProcessCorruptedStateExceptionsAttribute

---
+++ antinet/HandleProcessCorruptedStateExceptionsAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace System.Runtime.ExceptionServices {
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+	class HandleProcessCorruptedStateExceptionsAttribute : Attribute {
+	}
+}
