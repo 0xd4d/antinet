01105ed46fcbc807337525417f982401c8e601fd
de4dot <de4dot@gmail.com>
2013-01-02 18:17:11 +0100

    Print CLR version

--- antinet-test/AntiNetTest.cs
+++ antinet-test/AntiNetTest.cs
@@ -1,6 +1,12 @@
-﻿namespace antinet_test {
+﻿using System;
+
+namespace antinet_test {
 	public class AntiNetTest {
 		public static int main(string[] args) {
+			Console.WriteLine("CLR: {0} - {1}", Environment.Version, IntPtr.Size == 4 ? "x86" : "x64");
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+
 			return 0;
 		}
 	}
