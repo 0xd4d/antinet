425e52efdd3a0ff7b150f21fa4639d0c6832d3c0
de4dot <de4dot@gmail.com>
2013-01-05 07:55:53 +0100

    Add anti-managed profiler test code

--- antinet-test/AntiNetTest.cs
+++ antinet-test/AntiNetTest.cs
@@ -17,7 +17,13 @@ namespace antinet_test {
 			Console.ReadKey();
 			Console.WriteLine(sep);
 
-			// Here we go...
+			if (AntiManagedProfiler.Initialize())
+				Console.WriteLine("Anti-managed profiler code has been successfully initialized");
+			else {
+				Console.Error.WriteLine(seperr);
+				Console.Error.WriteLine("FAILED TO INITIALIZE ANTI-PROFILER CODE");
+				Console.Error.WriteLine(seperr);
+			}
 			if (AntiManagedDebugger.Initialize())
 				Console.WriteLine("Anti-managed debugger code has been successfully initialized");
 			else {
@@ -25,18 +31,50 @@ namespace antinet_test {
 				Console.Error.WriteLine("FAILED TO INITIALIZE ANTI-DEBUGGER CODE");
 				Console.Error.WriteLine(seperr);
 			}
+			Console.WriteLine();
 
 			Console.WriteLine("Try to attach a managed debugger/profiler or do it before this program starts.");
 			Console.WriteLine("Check whether you can set breakpoints, step over code, etc... :)");
+			ReportProfilerInfo();
 			Console.WriteLine("Press any key to exit...");
 			Console.ReadKey();
 			Console.WriteLine(sep);
+			GenerateProfilerEvents();
 
 			Console.WriteLine("Let's exit. Press any key (again!)...");
+			ReportProfilerInfo();
 			Console.ReadKey();
 			Console.WriteLine(sep);
 
 			return 0;
+		}
+
+		static void GenerateProfilerEvents() {
+			for (int i = 0; i < 10000; i++)
+				CastIt(i);
+		}
+
+		static void CastIt(object o) {
+			try {
+				string s = (string)o;
+			}
+			catch (InvalidCastException) {
+			}
+		}
+
+		static void ReportProfilerInfo() {
+			if (AntiManagedProfiler.IsProfilerAttached) {
+				Console.WriteLine("A profiler is connected!");
+
+				// NB: It's better to terminate the program since the profiler can still
+				// call into the CLR and do stuff to your program.
+				AntiManagedProfiler.PreventActiveProfilerFromReceivingProfilingMessages();
+				Console.WriteLine("Prevented the profiler from receiving any messages");
+				if (AntiManagedProfiler.IsProfilerAttached)
+					Console.WriteLine("Couldn't prevent the profiler from receiving any messages");
+			}
+			else
+				Console.WriteLine("A profiler is not connected!");
 		}
 	}
 }
