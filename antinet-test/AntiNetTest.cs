cf8ea82adeb947c015b745af8cf9256c062c6492
de4dot <de4dot@gmail.com>
2013-01-08 12:13:51 +0100

    Update test code

--- antinet-test/AntiNetTest.cs
+++ antinet-test/AntiNetTest.cs
@@ -14,8 +14,9 @@ namespace antinet_test {
 			Console.WriteLine("CLR: {0} - {1}", Environment.Version, IntPtr.Size == 4 ? "x86" : "x64");
 			Console.WriteLine();
 
-			Console.WriteLine("Press any key to install anti-managed debugger and anti-managed profiler code...");
+			Console.WriteLine("Profiler attached: can't determine that yet");
 			Console.WriteLine("Debugger.IsAttached: {0}", Debugger.IsAttached);
+			Console.WriteLine("Press any key to install anti-managed debugger and anti-managed profiler code...");
 			Console.ReadKey();
 			Console.WriteLine(sep);
 
@@ -35,21 +36,31 @@ namespace antinet_test {
 			}
 			Console.WriteLine();
 
-			Console.WriteLine("Try to attach a managed debugger/profiler or do it before this program starts.");
-			Console.WriteLine("Check whether you can set breakpoints, step over code, etc... :)");
+			Console.WriteLine("Profiler attached: {0}", AntiManagedProfiler.IsProfilerAttached);
+			if (AntiManagedProfiler.IsProfilerAttached) {
+				Console.WriteLine("Preventing the profiler from receiving messages.");
+
+				// NB: It's better to terminate the program since the profiler can still
+				// call into the CLR and do stuff to your program.
+				AntiManagedProfiler.PreventActiveProfilerFromReceivingProfilingMessages();
+				Console.WriteLine("Prevented the profiler from receiving messages");
+				Console.WriteLine("Profiler attached: {0} (at least that's what the CLR thinks)", AntiManagedProfiler.IsProfilerAttached);
+			}
 			Console.WriteLine("Debugger.IsAttached: {0}", Debugger.IsAttached);
-			ReportProfilerInfo();
-			Console.WriteLine("Press any key to exit...");
+			Console.WriteLine("Try to attach a debugger or profiler.");
+			Console.WriteLine("Press any key to execute some code for the profiler...");
 			Console.ReadKey();
 			Console.WriteLine(sep);
 			GenerateProfilerEvents();
 
+			Console.WriteLine("Profiler attached: {0}", AntiManagedProfiler.IsProfilerAttached);
 			Console.WriteLine("Debugger.IsAttached: {0}", Debugger.IsAttached);
 			Console.WriteLine("Let's exit. Press any key (again!)...");
-			ReportProfilerInfo();
 			Console.ReadKey();
 			Console.WriteLine(sep);
 
+			Console.WriteLine("Profiler attached: {0}", AntiManagedProfiler.IsProfilerAttached);
+			Console.WriteLine("Debugger.IsAttached: {0}", Debugger.IsAttached);
 			return 0;
 		}
 
@@ -64,21 +75,6 @@ namespace antinet_test {
 			}
 			catch (InvalidCastException) {
 			}
-		}
-
-		static void ReportProfilerInfo() {
-			if (AntiManagedProfiler.IsProfilerAttached) {
-				Console.WriteLine("A profiler is connected!");
-
-				// NB: It's better to terminate the program since the profiler can still
-				// call into the CLR and do stuff to your program.
-				AntiManagedProfiler.PreventActiveProfilerFromReceivingProfilingMessages();
-				Console.WriteLine("Prevented the profiler from receiving any messages");
-				if (AntiManagedProfiler.IsProfilerAttached)
-					Console.WriteLine("Couldn't prevent the profiler from receiving any messages");
-			}
-			else
-				Console.WriteLine("A profiler is not connected!");
 		}
 	}
 }
